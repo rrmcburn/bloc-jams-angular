41665451330aacd6073223433db093c46803f4de
Ryan McBurney <rrmcburn@gmail.com>
2017-02-19 16:08:14 -0500

    added controllers to bloc-jams - still need help with final task

---
+++ app/scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red'}, 
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink'},
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta'}
+     ]
+ };
+ 
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Ring, ring, ring', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Fits in your pocket', duration: 268.45, audioUrl: 'assets/music/red'},
+         { title: 'Can you hear me now?', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Wrong phone number', duration: 374.22, audioUrl: 'assets/music/magenta'}
+     ]
+ };
+
+var albumMcBurney = {
+     title: 'McBurney Life',
+     artist: 'Ryan McBurney',
+     label: 'DC',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/04.png',
+     songs: [
+         { title: 'Go Nats', duration: '4:26' },
+         { title: 'Go Redskins', duration: '3:14' },
+         { title: 'Go Wizards', duration: '5:01' },
+         { title: 'Go Caps', duration: '3:21'},
+         { title: 'Go Syracuse', duration: '2:15'}
+     ]
+ };
