e5d294f16f8a2d4e155486219108febece0e4711
Ryan McBurney <rrmcburn@gmail.com>
2017-02-25 14:02:17 -0500

    angular to update seek bar as time of song is changed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-    function SongPlayer(Fixtures) {
+    function SongPlayer($rootScope, Fixtures) {
      var SongPlayer = {};
         
  /**
@@ -33,7 +33,13 @@
             preload: true
         });
  
-    SongPlayer.currentSong = song;
+        currentBuzzObject.bind('timeupdate', function() {
+         $rootScope.$apply(function() {
+             SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });    
+        
+        SongPlayer.currentSong = song;
  };
         
 // * @function playSong
@@ -74,6 +80,13 @@
  */
     
     SongPlayer.currentSong = null;
+        
+        
+        /**
+ * @desc Current playback time (in seconds) of currently playing song
+ * @type {Number}
+ */
+ SongPlayer.currentTime = null;
         
         
         
@@ -142,10 +155,21 @@
      }
  };
         
+        /**
+ * @function setCurrentTime
+ * @desc Set current time (in seconds) of currently playing song
+ * @param {Number} time
+ */
+      SongPlayer.setCurrentTime = function(time) {
+        if (currentBuzzObject) {
+         currentBuzzObject.setTime(time);
+        }
+};
+        
      return SongPlayer;
  }
  
      angular
          .module('blocJams')
-         .factory('SongPlayer', ['Fixtures', '$rootScope', SongPlayer]);
+         .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
  })();
